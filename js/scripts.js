d5b524e121d35f7d83b196689eba18dbf265e29d
Kocki0 <filip.kocijan13@gmail.com>
2023-03-03 13:37:35 +0100

    Made it because I merged 2 repositories acidentaly

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+console.log("Yo,welcome to my site!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle'); 
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
+const fading = document.querySelectorAll(".slideright");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+fading.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
